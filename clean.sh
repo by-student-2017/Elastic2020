56433cabe73beaf4a406f4c59f8c43afe4a47548
By STUDENT <33742249+by-student-2017@users.noreply.github.com>
2025-08-18 14:12:42 +0900

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -2,4 +2,6 @@
 
 rm -fr Dst* Structures_ESPRESSO
 rm -f  Distorted_Parameters ElaStic_PW.in INFO_ElaStic sgroup.out
-rm -f  case.elastic.in set_stress.txt
+rm -f  case.elastic.in 
+rm -f set_energy_2nd.txt set_energy_3rd.txt
+rm -f set_stress_2nd.txt set_stress_3rd.txt
